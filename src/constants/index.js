04880fdd4cdf0f0f6ec50d184dd1161cfd77595f
Sugoto Basu <60142374+Sugoto@users.noreply.github.com>
2023-10-23 15:08:29 +0530

    Update Work Experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,55 +116,55 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Machine Learning Intern",
+    title: "Machine Learning Engineer Intern",
     company_name: "IIT Roorkee",
     icon: redux,
     iconBg: "#383E56",
     date: "Nov 2022 - March 2023",
     points: [
-      "Developed and implemented an efficient NLP pipeline that achieved an accuracy rate of 65% in transliterating documents from mixed Hinglish to pure languages.",
-      "Used advanced encoder-decoder architecture with fine tuned Hugging Face transformers.",
-      "Collaborated with a team comprising an eminent professor and mentors from IIT Roorkee, working with the National Informatics Centre (NIC), Government of India.",
+      "Accomplished a 65% accuracy in transliterating mixed Hinglish documents to pure English and native languages by designing and developing an efficient NLP pipeline, thereby streamlining language conversion processes.",
+      "Collaborated with an eminent professor and mentors from IIT Roorkee, working with the National Informatics Centre (NIC) of the Government of India, fostering a synergistic team approach.",
     ],
   },
   {
-    title: "GitHub Technical Manager",
-    company_name: "DIVERSION 2K22 @ IEM, Kolkata",
+    title: "Software Engineer Intern",
+    company_name: "PHOTOGPT AI",
     icon: git,
     iconBg: "#E6DEDD",
-    date: "Dec 2021 - May 2022",
+    date: "July 2023 – September 2023",
     points: [
-      "Managed and coordinated the efforts of 5 teams, overseeing their GitHub repositories, driving successful and timely contributions to the event.",
+      "Improved user functionality by integrating Large Language Models (LLMs) into Python applications with chatbots, and by deploying 7+ backend RESTful API endpoints.",
+      "Enhanced AI image quality by 15% through the implementation of Stable Diffusion and ControlNet in an end-to-end image generation pipeline and enabling the parallel creation of 5-10 images.",
     ],
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//     name: "Sara Lee",
+//     designation: "CFO",
+//     company: "Acme Co",
+//     image: "https://randomuser.me/api/portraits/women/4.jpg",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//     name: "Chris Brown",
+//     designation: "COO",
+//     company: "DEF Corp",
+//     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//   },
+//   {
+//     testimonial:
+//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Lisa Wang",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image: "https://randomuser.me/api/portraits/women/6.jpg",
+//   },
+// ];
 
 const projects = [
   {
